2fc65b07ad2d8de7dec537ee3001b64411625282
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-03-09 15:56:53 -0700

    creating objects for row content of table

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// connecting to MySQL database
+const mysql = require('mysql2');
+
 // create the express connection
 const express = require('express');
 const PORT = process.env.PORT || 3001;
@@ -7,12 +10,32 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+// connect to mysql2 database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySql password,
+        password: 'Greenocean-1',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 // GET test to test express connection
 // app.get('/', (req, res) => {
 //     res.json({
 //         message: 'Hello World'
 //     });
 // }); // run by npm start after adding start:node server.js to package.json
+
+
+
+// returns all data in candidates table
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // default response for any other request (Not Found)
 // this route overrides all others (Keep as last)
