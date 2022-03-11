dc7e0c57b3697ff4e0a1fffaebe5b41dd30db26e
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-03-10 17:41:23 -0700

    add CRUD routes

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 // connecting to MySQL database
 const mysql = require('mysql2');
 
+// input function to check input for post request
+const inputCheck = require('./utils/inputCheck');
+
 // create the express connection
 const express = require('express');
+const res = require('express/lib/response');
+const { run } = require('jest');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -30,38 +35,98 @@ const db = mysql.createConnection(
 //     });
 // }); // run by npm start after adding start:node server.js to package.json
 
-// create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-            VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-
-db.query(sql, params, (err, result) => {
-    if(err) {
-        console.log(err);
-    }
-    console.log(result);
-});
 
 // delete a candidate
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//     if(err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+app.delete('/api/candidate/:id', (req, res) => {
+    // creating var to delete a specified candidate
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    // accepting user input on which candidate to delete
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        // handling error
+        if(err) { // display message if candidate not found
+            res.statusMessage(400).json({error: res.message});
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else { // display message if properly deleted
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+// create a candidate using post request
+app.post('/api/candidate', ({body}, res) => {
+    // checking input
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    // handle error if input incorrect
+    if(errors) {
+        res.status(400).json({error: errors});
+        return;
+    }
+    // create a candidate
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+                VALUES (?,?,?)`;
+    // assigning request body values to variable
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    
+    db.query(sql, params, (err, result) => {
+        // handle error
+        if(err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        // display message and data to screen
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
 
 // GET a single candidate
-// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//     if(err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// });
+app.get('/api/candidate/:id', (req, res) => {
+    // creating a var to select a single candidate
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    // accepting user input on which candidate to pull
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+        // handling error
+        if(err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        // displaying message and data to screen
+        res.json({
+            message: 'sucess',
+            data: row
+        });
+    });
+});
 
-// returns all data in candidates table
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// });
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    // creates variable to return all data in candidates table
+    const sql = `SELECT * FROM candidates`;
+
+    db.query(sql, (err, rows) => {
+        // handle error
+        if(err) {
+            res.status(500).json({error: err.message});
+            return;
+        }
+        // displaying message and data to screen
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
 
 // default response for any other request (Not Found)
 // this route overrides all others (Keep as last)
